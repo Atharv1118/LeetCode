7d41566c96729d601156136cf40abeb5d432024e
Atharv Yande <yandeatharv022@gmail.com>
2026-08-12 17:20:41 +0530

    Time: 1 ms (66.46%), Space: 43.1 MB (22.49%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+       int balance = 0;
+       int ans = 0;
+        for(int i=0; i<s.length(); i++){
+
+            if(s.charAt(i) == '('){
+               balance++;
+            }
+            else{
+                if(balance > 0){
+                    balance--;
+                }
+                else{
+                    ans++;
+                }
+            }
+        }
+      return balance += ans;
+    }
+}
