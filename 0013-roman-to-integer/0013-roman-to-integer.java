4ae91d42c7e874206fe4ab6d8303703eb91731c3
Atharv Yande <yandeatharv022@gmail.com>
2026-08-17 22:07:04 +0530

    Time: 3 ms (77.63%), Space: 46.3 MB (86.16%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,27 +1,34 @@
 class Solution {
     public int romanToInt(String s) {
         int ans = 0;
-        HashMap<Character , Integer> map = new HashMap<>();
-        map.put('I' , 1);
-        map.put('V' , 5);
-        map.put('X' , 10);
-        map.put('L' , 50);
-        map.put('C' , 100);
-        map.put('D' , 500);
-        map.put('M' , 1000);
+        
+        for(int i=0 ; i<s.length(); i++){
 
-        for(int i=0; i<s.length(); i++){
             if(i == s.length()-1) {
-                ans += map.get(s.charAt(i));
+                ans += getValue(s.charAt(i));
             }
-            else if(map.get(s.charAt(i)) < map.get(s.charAt(i + 1))){
-                ans -= map.get(s.charAt(i));
+            else if(getValue(s.charAt(i)) < getValue(s.charAt(i + 1))){
+                
+                ans -= getValue(s.charAt(i));
             }
             else{
-                ans += map.get(s.charAt(i));
-            }
 
+                ans += getValue(s.charAt(i));
+            }
         }
-        return ans;
+        
+     return ans;
     }
+    public int getValue(char ch){
+            switch(ch){
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
+            }
+        }
 }
