64a138802d5edcf4e245b6b87caae900de1b3ea8
Atharv Yande <yandeatharv022@gmail.com>
2026-09-11 00:09:22 +0530

    Time: 12 ms (30.14%), Space: 59.3 MB (76.11%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -8,34 +8,71 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ 
+
 class Solution {
-    public ListNode sortList(ListNode head) {
-        
-        if(head == null || head.next == null) return head;
-        ArrayList<Integer> arr = new ArrayList<>();
+    public ListNode getMid(ListNode head){
 
-        // Pointer to traverse the list
-        ListNode temp = head;
+    ListNode slow = head;
+    ListNode fast = head;
 
-        // Traverse and push values into list
-        while (temp != null) {
-            arr.add(temp.val);
-            temp = temp.next;
-        }
+    while(fast.next != null && fast.next.next != null){
 
-        // Sort the list
-        Collections.sort(arr);
-
-        // Reassign sorted values to list nodes
-        temp = head;
-        for (int i = 0; i < arr.size(); i++) {
-            temp.val = arr.get(i);
-            temp = temp.next;
-        }
-
-        // Return head of sorted list
-        return head;
-
-
+        slow = slow.next;
+        fast = fast.next.next;
     }
+    return slow;
+}
+
+public ListNode merge(ListNode head1 , ListNode head2){
+
+    ListNode dummy = new ListNode(-1);
+    ListNode temp = dummy;
+
+    while(head1 != null && head2 != null){
+
+        if(head1.val < head2.val){
+            temp.next = head1;
+            
+            head1 = head1.next;
+        }
+         else{
+            temp.next = head2;
+            
+            head2 = head2.next;
+        }
+        temp = temp.next;
+    }
+        while(head1 != null ){
+
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        } 
+        while(head2 != null ){
+
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        } 
+        return dummy.next;
+}
+
+public ListNode sortList(ListNode head){
+
+    if(head == null || head.next == null) return head;
+    // Find the Mid
+    ListNode mid = getMid(head);
+
+    // Find the left anf right half
+    ListNode rightHead = mid.next;
+    mid.next = null;
+ 
+    ListNode head1 = sortList(head); // dividing left half
+    ListNode head2 = sortList(rightHead);// dividing right half
+    
+    //merge 
+    return merge(head1 , head2);
+}
+    
 }
