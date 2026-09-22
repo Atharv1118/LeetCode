846d54871f37e878f4f48cb2268b510b6a4bcf97
Atharv Yande <yandeatharv022@gmail.com>
2026-09-23 00:35:59 +0530

    Time: 3 ms (5.13%), Space: 42.9 MB (51.8%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,27 +1,50 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-            (a , b) -> b - a
-        );
-       
-        for(int i = 0; i<stones.length ; i++){
 
-            pq.add(stones[i]);
+        ArrayList<Integer> list = new ArrayList<>();
 
+        // Put all stones into ArrayList
+        for (int stone : stones) {
+            list.add(stone);
         }
-        while(pq.size() > 1){
 
-            int largest = pq.poll();
-            int seclargest = pq.poll();
+        while (list.size() > 1) {
 
-            if(largest != seclargest){
-                pq.add(largest - seclargest);
+            // Find largest stone
+            int largestIndex = 0;
+
+            for (int i = 1; i < list.size(); i++) {
+                if (list.get(i) > list.get(largestIndex)) {
+                    largestIndex = i;
+                }
+            }
+
+            // Remove largest stone
+            int largest = list.remove(largestIndex);
+
+            // Find second largest stone
+            int secondLargestIndex = 0;
+
+            for (int i = 1; i < list.size(); i++) {
+                if (list.get(i) > list.get(secondLargestIndex)) {
+                    secondLargestIndex = i;
+                }
+            }
+
+            // Remove second largest stone
+            int secondLargest = list.remove(secondLargestIndex);
+
+            // If stones are different, put the difference back
+            if (largest != secondLargest) {
+                list.add(largest - secondLargest);
             }
         }
-        if(!pq.isEmpty()){
-            return pq.poll();
+
+        // If one stone remains, return it
+        if (list.size() == 1) {
+            return list.get(0);
         }
+
         return 0;
     }
 }
